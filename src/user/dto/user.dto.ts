959ee9032b461de76c2b686f5ae2c40bb5f1561d
MatheusAlves <matpr.a98@gmail.com>
2024-08-18 18:59:10 -0300

    Tratando mensagens de erro

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -2,19 +2,19 @@ import { IsEmail, IsNotEmpty, IsPhoneNumber, IsString } from "class-validator"
 
 export class CreateUserDto {
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O nome é obrigatório!' })
   fullName: string;
 
   @IsEmail({}, { message: 'O email deve ser válido!' })
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O email é obrigatório!' })
   email: string;
 
   @IsString()
-  @IsNotEmpty()
-  @IsPhoneNumber('BR')
+  @IsNotEmpty({ message: 'O telefone é obrigatório!' })
+  @IsPhoneNumber('BR', {message: 'O telefone deve ser válido'})
   phone: string;
 
   @IsString()
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'O empresa é obrigatória!' })
   business: string
 }
